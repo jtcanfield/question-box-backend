04d57c4ba252168b605ccbf472c3aeb2360f5044
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-03 12:33:39 -0400

    add testing post

--- app.js
+++ app.js
@@ -1,65 +1,77 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const mongoURL = 'mongodb://databaseeditor:letsedit@ds139904.mlab.com:57971/questionbox';
+const mongoURL = 'mongodb://databaseeditor:letsedit@ds157971.mlab.com:57971/questionbox';
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://databaseeditor:letsedit@ds139904.mlab.com:57971/questionbox');
+mongoose.connect('mongodb://databaseeditor:letsedit@ds157971.mlab.com:57971/questionbox');
 //Db.prototype.authenticate method will no longer be available in the next major release 3.x as MongoDB 3.6 will only allow auth against users in the admin db and will no longer allow multiple credentials on a socket. Please authenticate using MongoClient.connect with auth credentials.
+
+// app.use(express.bodyParser());
+app.use(bodyParser.json());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.post("/test", upload.array(), function (req, res, next) {
+  console.log(req.body);
+});
 app.post("/stats/:data", function (req, res) {
   MongoClient.connect(mongoURL, function (err, db) {
-    const statsdb = db.collection("statistics");
-    statsdb.insertOne(JSON.parse(req.params.data));
+    const statsdb = db.collection("questions");
+    // statsdb.insertOne(JSON.parse(req.params.data));
   })
 });
-app.get("/stats", function (req, res) {
-  MongoClient.connect(mongoURL, function (err, db) {
-    const statsdb = db.collection("statistics");
-    statsdb.find().toArray(function (err, docs) {
-      return res.json(docs);
-    })
-  })
+/*
+        Prefix Verb   URI Pattern              Controller#Action
+        questions GET    /questions(.:format)     questions#index
+                  POST   /questions(.:format)     questions#create
+         question GET    /questions/:id(.:format) questions#show
+                  PATCH  /questions/:id(.:format) questions#update
+                  PUT    /questions/:id(.:format) questions#update
+                  DELETE /questions/:id(.:format) questions#destroy
+          answers GET    /answers(.:format)       answers#index
+                  POST   /answers(.:format)       answers#create
+           answer GET    /answers/:id(.:format)   answers#show
+                  PATCH  /answers/:id(.:format)   answers#update
+                  PUT    /answers/:id(.:format)   answers#update
+                  DELETE /answers/:id(.:format)   answers#destroy
+question_answers  GET    /questions/:question_id/answers(.:format)     answers#index
+                  POST   /questions/:question_id/answers(.:format)     answers#create
+  question_answer GET    /questions/:question_id/answers/:id(.:format) answers#show
+                  PATCH  /questions/:question_id/answers/:id(.:format) answers#update
+                  PUT    /questions/:question_id/answers/:id(.:format) answers#update
+                  DELETE /questions/:question_id/answers/:id(.:format) answers#destroy
+                 */
+/*
+app.get("/:dynamicsolo", function (req, res) {
+ MongoClient.connect(mongoURL, function (err, db) {
+   const statsdb = db.collection("statistics");
+   statsdb.find().toArray(function (err, docs) {
+     return res.json(docs);
+   })
+ })
 });
-app.listen(process.env.PORT || 5000);
-MongoClient.connect(mongoURL, function(err, db) {
-  if (err){
-    console.log("ERROR");
-    console.log(err);
-  }
+app.get("/:dynamic1/:dynamic2", function (req, res) {
+ MongoClient.connect(mongoURL, function (err, db) {
+   const statsdb = db.collection("statistics");
+   statsdb.find().toArray(function (err, docs) {
+     return res.json(docs);
+   })
+ })
+});
+*/
+app.listen(/*process.env.PORT || */5000);
+  MongoClient.connect(mongoURL, function(err, db) {
+    if (err){
+      console.log("ERROR");
+      console.log(err);
+    }
   db.close();
 });
-
-/*
-Prefix Verb   URI Pattern              Controller#Action
-questions GET    /questions(.:format)     questions#index
-          POST   /questions(.:format)     questions#create
- question GET    /questions/:id(.:format) questions#show
-          PATCH  /questions/:id(.:format) questions#update
-          PUT    /questions/:id(.:format) questions#update
-          DELETE /questions/:id(.:format) questions#destroy
-  answers GET    /answers(.:format)       answers#index
-          POST   /answers(.:format)       answers#create
-   answer GET    /answers/:id(.:format)   answers#show
-          PATCH  /answers/:id(.:format)   answers#update
-          PUT    /answers/:id(.:format)   answers#update
-          DELETE /answers/:id(.:format)   answers#destroy
-question_answers GET    /questions/:question_id/answers(.:format)     answers#index
-                 POST   /questions/:question_id/answers(.:format)     answers#create
- question_answer GET    /questions/:question_id/answers/:id(.:format) answers#show
-                 PATCH  /questions/:question_id/answers/:id(.:format) answers#update
-                 PUT    /questions/:question_id/answers/:id(.:format) answers#update
-                 DELETE /questions/:question_id/answers/:id(.:format) answers#destroy
-       questions GET    /questions(.:format)                          questions#index
-                 POST   /questions(.:format)                          questions#create
-        question GET    /questions/:id(.:format)                      questions#show
-                 PATCH  /questions/:id(.:format)                      questions#update
-                 PUT    /questions/:id(.:format)                      questions#update
-                 DELETE /questions/:id(.:format)                      questions#destroy
-                 /*
