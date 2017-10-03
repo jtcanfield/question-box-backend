eb93b9c7ff6a0beda692932f68f1a387e37fc768
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-03 14:28:30 -0400

    registration is working

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const LocalStrategy = require('passport-local').Strategy;//Not in use
 const expressValidator = require('express-validator');
 const models = require("./models");
 const QuestionModel = models.Question;
+const UserModel = models.User;
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const mongoURL = 'mongodb://databaseeditor:letsedit@ds157971.mlab.com:57971/questionbox';
@@ -15,8 +16,10 @@ mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://databaseeditor:letsedit@ds157971.mlab.com:57971/questionbox');
 //Db.prototype.authenticate method will no longer be available in the next major release 3.x as MongoDB 3.6 will only allow auth against users in the admin db and will no longer allow multiple credentials on a socket. Please authenticate using MongoClient.connect with auth credentials.
 
-// app.use(express.bodyParser());
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.text());
+app.use(expressValidator());
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -64,21 +67,49 @@ app.use(passport.session());
 
 
 
-app.post("/register", function (req, res, next) {
-  const question = new QuestionModel({
-            title: req.body.title,
-            language: req.body.language,
-            question: req.body.question,
-            tags: req.body.tags,
-            user: "user unavaiable",
-            solved: false,
-          })
-          console.log(question);
-  res.json(question);
-  // MongoClient.connect(mongoURL, function (err, db) {
-  //   const questionlisting = db.collection("questions");
-  //   questionlisting.insertOne(JSON.parse(req.params.data));
-  // })
+app.post('/register', function(req, res) {
+    req.checkBody('username', 'Username must be alphanumeric').isAlphanumeric();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Type Password Again').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Invalid Email').isEmail();
+    req.checkBody('password', 'Passwords do not match').equals(req.body.password2);
+    req.getValidationResult()
+        .then(function(result) {
+            if (!result.isEmpty()) {
+              console.log(result.mapped());
+                // return res.render("signup", {
+                //     username: req.body.username,
+                //     errors: result.mapped()
+                // });
+                return res.json("NOOOOOPE");
+            }
+            const user = new UserModel({
+                username: req.body.username,
+                name: req.body.name,
+                password: req.body.password2,
+                email: req.body.email
+            })
+            const error = user.validateSync();
+            if (error) {
+                return res.render("signup", {
+                    errors: normalizeMongooseErrors(error.errors)
+                })
+            }
+            console.log(user);
+            // user.save(function(err) {
+            //     if (err) {
+            //         return res.render("signup", {
+            //             messages: {
+            //                 error: ["That username is already taken."]
+            //             }
+            //         })
+            //     }
+            //     return res.redirect('/');
+            // })
+            res.json("got it");
+        })
 });
 
 
@@ -117,45 +148,6 @@ question_answers  GET    /questions/:question_id/answers(.:format)     answers#i
                   PATCH  /questions/:question_id/answers/:id(.:format) answers#update
                   DELETE /questions/:question_id/answers/:id(.:format) answers#destroy
                  */
-app.post('/signup/', function(req, res) {
-   req.checkBody('username', 'Username must be alphanumeric').isAlphanumeric();
-   req.checkBody('username', 'Username is required').notEmpty();
-   req.checkBody('password', 'Password is required').notEmpty();
-   req.checkBody('password2', 'Type Password Again').notEmpty();
-   req.checkBody('email', 'Email is required').notEmpty();
-   req.checkBody('email', 'Invalid Email').isEmail();
-   req.checkBody('password', 'Passwords do not match').equals(req.body.password2);
-   req.getValidationResult()
-       .then(function(result) {
-           if (!result.isEmpty()) {
-               return res.render("signup", {
-                   username: req.body.username,
-                   errors: result.mapped()
-               });
-           }
-           const user = new User({
-               username: req.body.username,
-               password: req.body.password,
-               email: req.body.email
-           })
-           const error = user.validateSync();
-           if (error) {
-               return res.render("signup", {
-                   errors: normalizeMongooseErrors(error.errors)
-               })
-           }
-           user.save(function(err) {
-               if (err) {
-                   return res.render("signup", {
-                       messages: {
-                           error: ["That username is already taken."]
-                       }
-                   })
-               }
-               return res.redirect('/');
-           })
-       })
-});
 
 
 
