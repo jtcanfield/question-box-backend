4ff3116c36647522962296de32e40810a592cef9
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-10-05 19:06:20 -0400

    add basic logout

--- app.js
+++ app.js
@@ -88,6 +88,15 @@ app.post('/checklogin', function(req, res, next) {
     }
   })
 });
+app.post('/logout', function(req, res, next) {
+  UserModel.findOne({ sessionID: req.sessionID }, function (err, person) {
+    if (person){
+      person.sessionID = '';
+      person.save();
+      return res.status(200).send();
+    }
+  })
+});
 
 app.post('/register', function(req, res) {
   req.checkBody('username', 'Username must be alphanumeric').isAlphanumeric();
